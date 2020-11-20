999e41ec559f0fed417cec6ba03cfa44a746ef15
guoj <guoj@landicorp.com>
2020-11-20 15:31:12 +0800

    增加跳转逻辑

--- app/src/main/java/com/guoj/navigationdemo/fragment/LoginFragment.kt
+++ app/src/main/java/com/guoj/navigationdemo/fragment/LoginFragment.kt
@@ -1,10 +1,29 @@
 package com.guoj.navigationdemo.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.guoj.navigationdemo.R
 import com.guoj.navigationdemo.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment:BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_login
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        val navController = findNavController()
+        btn_toForgetPage.setOnClickListener {
+            navController.navigate(R.id.action_login_fragment_to_forget_fragment)
+        }
+        btn_toRegisterPage.setOnClickListener {
+            navController.navigate(R.id.action_login_fragment_to_registerFragment)
+        }
     }
 }
